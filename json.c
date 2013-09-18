3cefcdc6226ea057e947520496e6c4a266f97ece
Werner <wstoop@gmail.com>
2013-09-18 19:55:50 +0200

    Added the start of a JSON utility library. I've decided that my level editor will save its stuff in JSON to be interoperable with other software.

---
+++ json.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+#include "hash.h"
+
+int json_escape(const char *in, char *out, size_t len) {
+	int i = 0;
+	while(in[0]) {
+	}
+}
---
+++ json.h
@@ -0,0 +1,2 @@
+int json_escape(const char *in, char *out, size_t len);
+
