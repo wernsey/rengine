dd4c23dfebe8e4ff75ad10b4916d20df846bc8da
Werner <wstoop@gmail.com>
2013-12-26 11:01:25 +0200

    Bace is a program to encode a binary file as C source code. I need it bake Lua scripts into the Rengine executable.

---
+++ src/bace.c
@@ -0,0 +1,107 @@
+/*
+Bace: Binary As C Encoder
+Utility that takes a binary file as inputs and outputs it 
+as C code in the form of a char[].
+Pronounced 'Bake', because I need it to bake some binary 
+files into my executable.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+
+#define BUFFER_SIZE	1024
+
+int verbose = 0;
+
+void usage(const char *name) {
+	fprintf(stderr, "Usage: %s [options] infile\n", name);
+	fprintf(stderr, "where options:\n");
+	fprintf(stderr, " -o file     : Set the output file (default: stdout)\n");
+	fprintf(stderr, " -n name     : Name of the created variable.\n");
+	fprintf(stderr, " -z          : Append a '\\0' to to the end of the generated array.\n");
+	fprintf(stderr, " -v          : Verbose mode. Each -v increase verbosity.\n");
+}
+
+int main(int argc, char *argv[]) {
+	FILE *infile = NULL;
+	size_t len = 0, sublen, i;
+	FILE *outfile = stdout;
+	char *var_name = NULL, *c;
+	char buffer[BUFFER_SIZE];
+	int opt, append_z = 0;
+	while((opt = getopt(argc, argv, "o:n:zv?")) != -1) {
+		switch(opt) {	
+			case 'o': {
+				outfile = fopen(optarg, "w");
+				if(!outfile) {
+					fprintf(stderr, "error: unable to open %s for output\n", optarg);
+					return 1;
+				}
+			} break;
+			case 'n': {
+				var_name = strdup(optarg);
+			} break;
+			case 'z' : {
+				append_z = 1;
+			} break;
+			case 'v' : {
+				verbose++;
+			} break;
+			case '?' : {
+				usage(argv[0]);
+				return 1;
+			}
+		}
+	}
+	
+	if(optind >= argc) {
+		fprintf(stderr, "error: expected infile\n");
+		usage(argv[0]);
+		return 1;
+	}
+	
+	infile = fopen(argv[optind], "rb");
+	if(!infile) {
+		fprintf(stderr, "error: unable to open %s for input\n", argv[optind]);
+		return 1;
+	}
+	
+	if(!var_name) {
+		var_name = strdup(argv[optind]);
+	}
+	for(c = var_name; *c; c++) {
+		if(!isalnum(*c))
+			*c = '_';
+	}
+	
+	fprintf(outfile, "const char %s[] = {", var_name);
+	while(!feof(infile)) {
+		sublen = fread(buffer, 1, BUFFER_SIZE, infile);
+		for(i = 0; i < sublen; i++) {			
+			char byte = buffer[i];
+			if(len + i > 0) {
+				if((len + i) % 8 == 0) {
+					fprintf(outfile, ",\n  0x%02X", byte & 0xFF);
+				} else
+					fprintf(outfile, ", 0x%02X", byte & 0xFF);
+			}else
+				fprintf(outfile, "\n  0x%02X", byte & 0xFF);
+		}
+		len += sublen;
+	}
+	if(append_z) {
+		/* Appending a \0 is useful if the input file is actually a text file.
+			Having the null terminator allows you to treat the generated variable
+			as a string. */
+		fprintf(outfile, ",\n  0x00};\n");
+	} else
+		fprintf(outfile, "};\n");
+	fprintf(outfile, "size_t %s_len = %u;\n", var_name, len);
+	
+	free(var_name);
+	fclose(infile);
+	if(outfile != stdout) {
+		fclose(outfile);
+	}		
+	return 0;
+}
